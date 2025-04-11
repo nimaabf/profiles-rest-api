8785fd992983e6775c802fe17daec5bf86c4e7b6
nimaabf <abf.nima@gmail.com>
2025-04-11 17:48:30 +0330

    Set URLS & Hello Function

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView  # type: ignore
+from rest_framework.response import Response  # type: ignore
 # Create your views here.
+
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        an_apiview = ['Use Http method as function']
+        return Response({"message": 'Hello', 'an_apiview': an_apiview})
