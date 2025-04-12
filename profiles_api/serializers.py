cdbb4fb8cc3ff195311ef0a52e16b0e0970c5ed3
nimaabf <abf.nima@gmail.com>
2025-04-13 00:11:57 +0330

    Added Viewsets for createing User

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,28 @@
 from rest_framework import serializers
 from django.conf import settings
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.Serializer):
+
+    class Meta:
+        model = models.UserProfile
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView  # type: ignore
 from rest_framework.response import Response  # type: ignore
 from rest_framework import status  # type: ignore
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
+from profiles_api import models
 # Create your views here.
 
 
@@ -24,14 +26,50 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk=none):
+    def put(self, request, pk=None):
         # put functionality
         return Response({'method': "PUT"})
 
-    def patch(self, request, pk=none):
-        # put functionality
+    def patch(self, request, pk=None):
+        # patch functionality
         return Response({'method': "PATCH"})
 
-    def delete(self, request, pk=none):
-        # put functionality
+    def delete(self, request, pk=None):
+        # delete functionality
         return Response({'method': "DELETE"})
+
+
+class HelloViewSets(viewsets.ViewSet):
+
+    def list(self, request):
+        return Response({"message": "This is List Function"})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        # retrieve functionality
+        return Response({'method': "retrieve"})
+
+    def update(self, request, pk=None):
+        # retrieve functionality
+        return Response({'method': "update"})
+
+    def partial_update(self, request, pk=None):
+        # retrieve functionality
+        return Response({'method': "partial_update"})
+
+    def destroy(self, request, pk=None):
+        # retrieve functionality
+        return Response({'method': "destroy"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serilizer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
