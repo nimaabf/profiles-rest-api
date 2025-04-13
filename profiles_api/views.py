78dcd502377173a79a5ff1e7d9453acf3e3e1753
nimaabf <abf.nima@gmail.com>
2025-04-13 16:20:43 +0330

    Added Login A

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status  # type: ignore
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import permissions
@@ -79,3 +81,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
